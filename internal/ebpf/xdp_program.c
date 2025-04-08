5e72262303dd77ec98e02c79587973e153b8f2bb
knileshh <neutron_stays@protonmail.com>
2025-04-08 10:30:00 +0530

    feat(ebpf): add skeleton XDP program

---
+++ internal/ebpf/xdp_program.c
@@ -0,0 +1,13 @@
+﻿// SPDX-License-Identifier: GPL-2.0
+// XDP program for SSDP packet filtering
+
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+SEC("xdp")
+int xdp_ssdp_filter(struct xdp_md *ctx) {
+    // TODO: Implement packet filtering
+    return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
