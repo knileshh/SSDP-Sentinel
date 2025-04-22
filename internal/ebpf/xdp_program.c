5a5a9278ef3793ade720d5d64bcf55651f3ba5df
knileshh <neutron_stays@protonmail.com>
2025-04-22 15:00:00 +0530

    feat(ebpf): add Ethernet header parsing

--- internal/ebpf/xdp_program.c
+++ internal/ebpf/xdp_program.c
@@ -2,11 +2,21 @@
 // XDP program for SSDP packet filtering
 
 #include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
 #include <bpf/bpf_helpers.h>
 
 SEC("xdp")
 int xdp_ssdp_filter(struct xdp_md *ctx) {
-    // TODO: Implement packet filtering
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+    
+    // Parse Ethernet header
+    struct ethhdr *eth = data;
+    if ((void *)(eth + 1) > data_end)
+        return XDP_PASS;
+    
+    // TODO: Parse IP and UDP headers
     return XDP_PASS;
 }
 
