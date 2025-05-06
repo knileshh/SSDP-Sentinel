305a60c3f5dca921779785703cf38ed39127ab61
knileshh <neutron_stays@protonmail.com>
2025-05-06 11:15:00 +0530

    feat(ebpf): add blocklist map definition

--- internal/ebpf/xdp_program.c
+++ internal/ebpf/xdp_program.c
@@ -4,19 +4,28 @@
 #include <linux/bpf.h>
 #include <linux/if_ether.h>
 #include <linux/ip.h>
+#include <linux/udp.h>
 #include <bpf/bpf_helpers.h>
+
+// Blocklist map
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 10000);
+    __type(key, __u32);
+    __type(value, __u64);
+} blocklist SEC(".maps");
 
 SEC("xdp")
 int xdp_ssdp_filter(struct xdp_md *ctx) {
     void *data_end = (void *)(long)ctx->data_end;
     void *data = (void *)(long)ctx->data;
     
-    // Parse Ethernet header
+    // Parse headers
     struct ethhdr *eth = data;
     if ((void *)(eth + 1) > data_end)
         return XDP_PASS;
     
-    // TODO: Parse IP and UDP headers
+    // TODO: Check blocklist and filter
     return XDP_PASS;
 }
 
