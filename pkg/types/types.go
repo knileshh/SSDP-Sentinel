0f579cbd3f65daf4c6d05f2fd312eb4f26358283
knileshh <neutron_stays@protonmail.com>
2025-06-03 10:45:00 +0530

    feat(types): add PacketFeatures structure stub

---
+++ pkg/types/types.go
@@ -0,0 +1,19 @@
+﻿package types
+
+import "net"
+
+// PacketFeatures represents the 80 features for ML
+type PacketFeatures struct {
+    SourceIP   net.IP
+    DestIP     net.IP
+    SourcePort uint16
+    DestPort   uint16
+    Protocol   uint8
+    
+    // Flow statistics
+    FlowDuration      uint64
+    TotalFwdPackets   uint32
+    TotalBwdPackets   uint32
+    
+    // TODO: Add remaining 72 features
+}
