1aeec5df9911bfcae01da62f64371977cd6ebbd2
knileshh <neutron_stays@protonmail.com>
2025-07-01 11:30:00 +0530

    feat(ebpf): add eBPF loader skeleton

---
+++ internal/ebpf/loader.go
@@ -0,0 +1,27 @@
+﻿package ebpf
+
+import (
+    "github.com/cilium/ebpf"
+    "github.com/sirupsen/logrus"
+)
+
+// Loader manages eBPF program lifecycle
+type Loader struct {
+    iface string
+    log   *logrus.Logger
+}
+
+// NewLoader creates a new eBPF loader
+func NewLoader(iface string, logger *logrus.Logger) *Loader {
+    return &Loader{
+        iface: iface,
+        log:   logger,
+    }
+}
+
+// Load loads the eBPF objects
+func (l *Loader) Load() error {
+    l.log.Info("Loading eBPF objects...")
+    // TODO: Implement loading
+    return nil
+}
