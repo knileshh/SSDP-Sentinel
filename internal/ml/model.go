3cb0306ff0b41fccc6ecabb3cbd4eb9a165977e6
knileshh <neutron_stays@protonmail.com>
2025-08-05 10:00:00 +0530

    feat(ml): add ML model interface

---
+++ internal/ml/model.go
@@ -0,0 +1,16 @@
+﻿package ml
+
+import "github.com/sirupsen/logrus"
+
+// Model interface for ML predictions
+type Model interface {
+    LoadModel(path string) error
+    Predict(features []float64) (*Prediction, error)
+}
+
+// Prediction represents ML model output
+type Prediction struct {
+    IsMalicious bool
+    Confidence  float64
+    Label       string
+}
