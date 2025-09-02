a540c959947cc2ca97fed37bf59d58d7562fd2c2
knileshh <neutron_stays@protonmail.com>
2025-09-02 11:45:00 +0530

    feat(config): implement configuration loader

---
+++ internal/config/config.go
@@ -0,0 +1,11 @@
+﻿package config
+
+import (
+    "github.com/spf13/viper"
+)
+
+// LoadConfig loads configuration from file
+func LoadConfig(path string) error {
+    viper.SetConfigFile(path)
+    return viper.ReadInConfig()
+}
